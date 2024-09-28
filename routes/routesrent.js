06799a760d104965fc6932fac9e5c872c6be4a48
yulianabreuc <abreucarrilloyv@uvm.edu.ve>
2024-09-27 23:23:15 -0400

    agregado en router rent el endp por search id rent

--- routes/routesrent.js
+++ routes/routesrent.js
@@ -5,5 +5,6 @@ const Controller = require('../controllers/controllerRent.js');
 router.get('/', Controller.getRents);
 router.post('/', Controller.createRent);
 router.put('/:id', Controller.updateRent);
+router.get('/:id', Controller.getRentById);
 
 module.exports = router;
